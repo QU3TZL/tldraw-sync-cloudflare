4375018208715c390dbdcb473abb2493348f3cb5
N3wM4t1c <matt@rhombus.ventures>
2025-12-01 12:51:48 -0600

    Fix vite config to resolve @tldraw modules from shared folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 	return {
 		plugins: [cloudflare(), react()],
+		resolve: {
+			alias: {
+				// Resolve @tldraw modules from this project's node_modules
+				// This is needed because shared folder imports @tldraw modules
+				'@tldraw/editor': path.resolve(__dirname, 'node_modules/@tldraw/editor'),
+				'@tldraw/tldraw': path.resolve(__dirname, 'node_modules/@tldraw/tldraw'),
+				'@tldraw/tlschema': path.resolve(__dirname, 'node_modules/@tldraw/tlschema'),
+			},
+		},
 	}
 })
